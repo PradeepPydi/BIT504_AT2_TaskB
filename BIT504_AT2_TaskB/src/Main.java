1e03c582eae89a9c9b46e3eb280af9f4f1fdeb85
PradeepPydi <mr.pradeeppydi@localhost>
2022-05-18 21:35:02 +1200

    First Commit

---
+++ BIT504_AT2_TaskB/src/Main.java
@@ -0,0 +1,42 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Class Object Example\n");
+		 
+	    // Create an object from the Paddle class
+	    Paddle Paddle1 = new Paddle();
+	    // Set the properties of this object 
+	    // We should really use setters and getters here but for simplicity we are just using public variables of the class 
+	    // see encapsulation
+	    Paddle1.name = "Paddle1";
+	    Paddle1.height = 63;
+	    Paddle1.width = 155;
+	    Paddle1.color = "FF00FF";
+	 
+	    // Create an object from the Paddle class
+	    Paddle Paddle2 = new Paddle();
+	    // Set properties
+	    Paddle2.name = "Paddle2";
+	    Paddle2.height = 38;
+	    Paddle2.width = 261;
+	    Paddle2.color = "66D91";
+	 
+	  // Display the attributes of these new Objects  
+	 
+	  System.out.println("Name:   " + Paddle1.name);
+	  System.out.println("height: " + Paddle1.height);
+	  System.out.println("width:  " + Paddle1.width);
+	  System.out.println("Color:  " + Paddle1.color);
+	  System.out.println("");
+	 
+	  System.out.println("Name:   " + Paddle2.name);
+	  System.out.println("height: " + Paddle2.height);
+	  System.out.println("width:  " + Paddle2.width);
+	  System.out.println("Color:  " + Paddle2.color);
+	  System.out.println("");
+	 
+
+	}
+
+}
---
+++ BIT504_AT2_TaskB/src/Paddle.java
@@ -0,0 +1,8 @@
+
+public class Paddle {
+	public String name;
+	  public int height;
+	  public int width;
+	  public String color;		
+
+}
